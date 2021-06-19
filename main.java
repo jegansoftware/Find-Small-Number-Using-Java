92f33f0e8f9941ffe44660bcbecaef804f932a94
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-19 12:48:51 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+///file name should be named as sss.java
+
+package claass;
+
+public class sss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		J n=new J();
+		n.get();
+		n=null;
+
+	}
+
+}
